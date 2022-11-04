ba174a5c265cb70856e0b38dce442d77d7081c2f
Natalia <remark2707@gmail.com>
2022-11-04 23:40:53 +0300

    add files

---
+++ src/main/java/org/example/client/Main.java
@@ -0,0 +1,25 @@
+package org.example.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8090;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
+        {
+            out.println("Netology.ru");
+            String resp = in.readLine();
+            System.out.println(resp);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
