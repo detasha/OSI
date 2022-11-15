8a2c099d5fb755d55e94b4d071532495317bc2d3
Natalia <47963827+detasha@users.noreply.github.com>
2022-11-15 15:11:44 +0300

    Update Main.java
    chanch bracket position first

--- src/main/java/org/example/server/Main.java
+++ src/main/java/org/example/server/Main.java
@@ -15,8 +15,7 @@ public class Main {
             try (ServerSocket serverSocket = new ServerSocket(port);
                  Socket clientSocket = serverSocket.accept();
                  PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
-            {
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
                 System.out.printf("New connection accepted Port: %d%n", clientSocket.getPort());
                 final String name = in.readLine();
                 out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
