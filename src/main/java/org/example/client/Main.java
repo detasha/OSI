e77b34e79cfecc681d54a6694afa474ed0bba85d
Natalia <47963827+detasha@users.noreply.github.com>
2022-11-15 15:12:24 +0300

    Update Main.java
    chanch bracket position second

--- src/main/java/org/example/client/Main.java
+++ src/main/java/org/example/client/Main.java
@@ -12,8 +12,7 @@ public class Main {
         int port = 8090;
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
-        {
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
             out.println("Netology.ru");
             String resp = in.readLine();
             System.out.println(resp);
